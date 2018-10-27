42c982850c497822435e735e11d88a416f141a10
gitdante <92villarreal@gmail.com>
2018-10-27 22:30:30 +0800

    brute force and divide conquer

--- inversions.py
+++ inversions.py
@@ -1,15 +1,19 @@
-#Algo1
+# Author:ZERO
+
+
+# Algo1
 def brute_force_inversions(arr):
-    n=len(arr)
-    c=0
-    i=0
-    for i in range(n-1):
+    n = len(arr)
+    c = 0
+    i = 0
+    for i in range(n - 1):
         for j in arr[i:]:
-            if arr[i]>j:
-                c+=1
+            if arr[i] > j:
+                c += 1
     return c
 
-#Algo2
+
+# Algo2 divide conquer
 def inversions(arr):
     n = len(arr)
     if n == 1:
@@ -17,34 +21,35 @@ def inversions(arr):
     else:
         left = inversions(arr[:n // 2])
         right = inversions(arr[n // 2:])
-        cross = countsplit(arr)
+        cross = count_split(arr)
         total = left + right + cross
     return total
 
 
-def countsplit(arr):
+def count_split(arr):
     n = len(arr)
-    left=arr[:n//2]
+    left = arr[:n // 2]
     left.sort()
-    right=arr[n//2:]
+    right = arr[n // 2:]
     right.sort()
-    r=[]
-    i=0
-    j=0
-    c=0
+    r = []
+    i = 0
+    j = 0
+    c = 0
     for p in range(n):
         try:
             if left[i] < right[j]:
                 r.append(left[i])
-                i+=1
+                i += 1
             else:
                 r.append(right[j])
-                j+=1
-                c+=len(left)-i
+                j += 1
+                c += len(left) - i
         except:
             pass
     return c
 
-x=inversions([7,1,3,5,2,6,4])
-y=brute_force_inversions([7,1,3,5,2,6,4])
-print(x,y)
+
+x = inversions([7, 1, 3, 5, 2, 6, 4])
+y = brute_force_inversions([7, 1, 3, 5, 2, 6, 4])
+print(x, y)
